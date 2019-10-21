3a552feea888b184d72e4eb750b5207a02d8c50a
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2019-10-21 20:46:33 +1100

    Updated post.js template

--- src/components/layouts/post.js
+++ src/components/layouts/post.js
@@ -10,14 +10,16 @@ const Post = props => {
         wpgraphql: { post },
       },
     } = props
-    const { title, content, date, modified } = post
+    const { title, content, date, modified, postACF, categories } = post
     const modifiedTime = moment(modified).startOf().fromNow()
     const postedTime = moment(date).format("MMM Do YYYY")
+
     return (
         <Layout>
         <SEO title={title} />
         <div className="page__hero single__post">
             <div className="wrapper">
+            <div className="post__card-icon" style={{backgroundColor: postACF.iconColor}} dangerouslySetInnerHTML={{ __html: postACF.icon }} />
             <h1 className="page__title" dangerouslySetInnerHTML={{ __html: title }} />
             <p className="post__date" dangerouslySetInnerHTML={{ __html: `Posted on ` + postedTime }}/>
             <p className="post__date" dangerouslySetInnerHTML={{ __html: `Updated about ` + modifiedTime }}/>
@@ -41,6 +43,17 @@ export const pageQuery = graphql`
         modified
         content
         uri
+        categories{
+            edges{
+              node{
+                name
+              }
+            }
+        }
+        postACF {
+            icon
+            iconColor
+        }
       }
     }
   }
